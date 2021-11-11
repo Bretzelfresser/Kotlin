93ee33af34639d186e84d5ae8482181909e26699
Bretzelfresser <81638199+Bretzelfresser@users.noreply.github.com>
2021-11-11 21:50:58 +0100

    parser

--- src/main/kotlin/graph/Graph.kt
+++ src/main/kotlin/graph/Graph.kt
@@ -1,4 +1,75 @@
 package graph
 
-class Graph {
+import java.io.File
+
+
+data class Graph(val amountNodes : Int, val amountEdges : Int, val nodeList : Array<Node>) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Graph
+
+        if (amountNodes != other.amountNodes) return false
+        if (amountEdges != other.amountEdges) return false
+        if (!nodeList.contentEquals(other.nodeList)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = amountNodes
+        result = 31 * result + amountEdges.hashCode()
+        result = 31 * result + nodeList.contentHashCode()
+        return result
+    }
+
+    companion object {
+
+        var staticIsFun : Int = 0
+
+        fun parseGraph(path : String) : Graph{
+
+        var numNodes : Int = 0
+        var numEdges : Int = 0
+
+
+            // 0..5:    clear metadata
+            // 5:       num of nodes
+            // 6:       num of edges
+            // 7..?:    id, ignore, lat, long, ignore
+            val text = File(path).readText()
+            val lineList = text.split("\n")
+
+            try {
+                numNodes = lineList[5].toInt()
+                numEdges = lineList[6].toInt()
+            } catch (nfe: NumberFormatException) {
+                // not a valid int
+            }
+            var adjacencyList = Array(numNodes) { Node(1, 0.0, 0.0) }
+
+            var lineString : List<String>
+            val offset : Int = 6
+            for(i in 0..numNodes+1) {
+                lineString = lineList[i + offset].split(" ")
+
+                println(lineString)
+
+                adjacencyList[i + offset] = Node(
+                    lineString[0].toInt(),
+                    lineString[2].toDouble(),
+                    lineString[3].toDouble() )
+            }
+
+
+
+            return Graph(numNodes, numEdges, adjacencyList)
+        }
+    }
+
+
+
 }
+
--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,7 +1,14 @@
+import graph.Graph
+
+val GRAPH : Graph = Graph.Companion.parseGraph("somPath")
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println(Int.MAX_VALUE)
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+
+
 }
